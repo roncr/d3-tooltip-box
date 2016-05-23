5e5f49413e1ddf9affe1a71b5cbf69a3db2e9560
roncr <ronaldcr10@gmail.com>
2016-05-22 19:56:45 -0700

    Added data accessor and template compiler

--- src/tooltip.js
+++ src/tooltip.js
@@ -7,12 +7,14 @@ class Tooltip {
     constructor() {
         this._tooltipClass = 'd3-tooltip-box';
         this._transitionSpeed = 200;
+        this._dataAccessor = (d) => d.value;
+        this._template = "<div>${value}</div>";
 
+        // Gets/Creates tooltip
         let tooltipEl = d3.select(`.${this._tooltipClass}`);
         if(tooltipEl.empty()) {
             tooltipEl = this._createTooltip();
         }
-
         this._tooltipEl = tooltipEl;
     }
 
@@ -25,11 +27,35 @@ class Tooltip {
             });
     }
 
+    _compileTemplate (template, data) {
+        for(let prop in data) {
+            var regex = new RegExp(`\\$\\{${prop}\\}`, 'gi');
+            template = template.replace(regex, data[prop]);
+        }
+
+        return template;
+    }
+
+    data(v) {
+        console.log('data');
+        if(arguments.length == 0) {
+            return this._dataAccessor;
+        }
+
+        this._dataAccessor = v;
+        return this;
+    }
+
     show(d) {
+        var data = this._dataAccessor(d),
+            html = this._compileTemplate(this._template, data);
+        console.log(data);
+        this._tooltipEl
+            .html(html);
+
         this._tooltipEl
             .transition(this._transitionSpeed)
             .style("opacity", 1);
-        this._tooltipEl.html(`<div>${d.value}</div>`);
     }
 
     move() {
@@ -53,7 +79,7 @@ class Tooltip {
             selection.each(function(data) {
                 var element = d3.select(this);
 
-                element.on('mouseover.d3-tooltip-box', () => { self.show(); })
+                element.on('mouseover.d3-tooltip-box', (d) => { console.log('mouse over'); self.show(d); })
                     .on('mousemove.d3-tooltip-box', () => { self.move(); })
                     .on('mouseleave.d3-tooltip-box', () => { self.hide(); });
             });
