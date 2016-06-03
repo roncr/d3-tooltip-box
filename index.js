50ff609a9155ffc8b07a2e7f3418099007614416
roncr <ronaldcr10@gmail.com>
2016-06-02 23:10:23 -0700

    Added derequire into the build process so that the lib can be bundled again with browserify

--- index.js
+++ index.js
@@ -1,7 +1,19 @@
 import tooltip from './src/tooltip';
 import { configurationSetter as config } from './src/config';
 
-export {
+/*
+ * Notes:
+ * 1) module.exports is used instead of ES6 export, because Babel
+ * will create a d3Tooltip.default object, which is not convenient
+ * for browser usage.
+ *
+ * 2) derequire is used so that it can be bundled with browserify again
+ * see:
+ *  http://stackoverflow.com/a/28176927
+ *  https://github.com/substack/node-browserify/issues/374
+ *  https://github.com/substack/node-browserify/pull/1151
+ */
+module.exports = {
     tooltip,
     config
 }
